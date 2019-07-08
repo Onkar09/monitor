900f26b64402df694527b68507b7b638c1634fc4
Onkar09 <onkar@Onkars-MacBook-Pro.local>
2019-07-08 12:14:08 +0530

    require rspec

--- features/base.rb
+++ features/base.rb
@@ -1,8 +1,9 @@
 require 'selenium-webdriver'
 require 'yaml'
 require 'test/unit'
-require 'rspec/expectations'
-include RSpec::Matchers
+# require 'rspec/expectations'
+require 'rspec'
+# include RSpec::Matchers
 # require 'require_all'
 # require '/Users/onkar/Documents/cucumber_project/features/support/base'
 
@@ -11,13 +12,14 @@ include RSpec::Matchers
 class BasePage < Test::Unit::TestCase
 
 	def initialize
-		# Selenium::WebDriver::Chrome.driver_path='./features/Drivers/chromedriver'
+		Selenium::WebDriver::Chrome.driver_path='./features/Drivers/chromedriver'
 		# options = Selenium::WebDriver::Chrome::Options.new
 		# options.add_argument('--headless')
 		# @driver = Selenium::WebDriver.for :chrome, options: options
 		@driver = Selenium::WebDriver.for :chrome
 		# @driver = Selenium::WebDriver::for :firefox
-		@driver.manage.window.resize_to(1440,900)
+		# @driver.manage.window.resize_to(1440,900)
+		# @driver.manage.window.resize_to(411,823)
 		# @driver.manage.window.maximize
 	end
 
