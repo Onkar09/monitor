a274855d7f30c4214af0228162e3f4042b86a6b8
Onkar09 <onkar@Onkars-MacBook-Pro.local>
2019-07-08 16:46:29 +0530

    base chrome change

--- features/base.rb
+++ features/base.rb
@@ -19,7 +19,7 @@ class BasePage < Test::Unit::TestCase
 		chrome_options.add_argument('--headless')
 		chrome_options.add_argument('--no-sandbox')
 		chrome_options.add_argument('--disable-dev-shm-usage')
-		chrome_options.add_extension('w3c', false)
+		chrome_options.add_argument('w3c', false)
 		@driver = Selenium::WebDriver.for :chrome, options: chrome_options
 		@driver.manage.timeouts.implicit_wait = 120 # seconds
 		# @driver = Selenium::WebDriver::for :chrome
